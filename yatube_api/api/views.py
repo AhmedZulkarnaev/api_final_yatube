2734c50f69128bc498e4a97e7d9f74721c046fcd
Ahmed <ahmedzulkarnaev9500@gmail.com>
2023-12-29 21:40:41 +0300

    add mixin

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,26 +1,57 @@
-from rest_framework import viewsets
-from rest_framework.pagination import LimitOffsetPagination
-from rest_framework import permissions
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
+from rest_framework import permissions, status, viewsets
+from rest_framework.response import Response
+from rest_framework.pagination import LimitOffsetPagination
 
-from posts.models import Post, Comment, Group
-from .serializers import PostSerializer, CommentSerializer, GroupSerializer
+from posts.models import Comment, Follow, Group, Post
+from .serializers import (
+    CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer)
 
 
-class PostViewSet(viewsets.ModelViewSet):
+class AuthorPermissionMixin:
+    def handle_author_permission(self, instance, request):
+        if request.user != instance.author:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        response = self.handle_author_permission(instance, request)
+        if not response:
+            serializer = self.get_serializer(
+                instance, data=request.data, partial=partial
+            )
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+        return (
+            response
+            or Response(serializer.data, status=status.HTTP_200_OK)
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        response = self.handle_author_permission(instance, request)
+        if not response:
+            instance.delete()
+        return (
+            response or Response(status=status.HTTP_204_NO_CONTENT)
+        )
+
+
+class PostViewSet(AuthorPermissionMixin, viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
-class CommentViewSet(viewsets.ModelViewSet):
+class CommentViewSet(AuthorPermissionMixin, viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_post_object_or_404(self):
         post_id = self.kwargs.get('post_id')
@@ -38,4 +69,10 @@ class CommentViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class FollowtViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
     permission_classes = [permissions.IsAuthenticated]
