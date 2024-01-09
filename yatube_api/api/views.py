dbef31b61d18e934b29466ebc23eb8890f09e9b8
Ahmed <ahmedzulkarnaev9500@gmail.com>
2024-01-09 16:42:36 +0300

    update permissons

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,54 +1,20 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import filters, permissions, status, viewsets
+from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.response import Response
 from rest_framework import mixins
 
-from posts.models import Comment, Group, Post
+from api.permissons import IsAuthorOrReadOnly
+from posts.models import Group, Post
 from .serializers import (
     CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer)
 
 
-class AuthorPermissionMixin:
-    """Mixin для проверки доступа автора к объектам."""
-
-    def handle_author_permission(self, instance, request):
-        """Проверяет доступ автора к объекту."""
-        if request.user != instance.author:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-
-    def update(self, request, *args, **kwargs):
-        """Обновляет объект и проверяет доступ автора."""
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        response = self.handle_author_permission(instance, request)
-        if not response:
-            serializer = self.get_serializer(
-                instance, data=request.data, partial=partial
-            )
-            serializer.is_valid(raise_exception=True)
-            self.perform_update(serializer)
-        return (
-            response
-            or Response(serializer.data, status=status.HTTP_200_OK)
-        )
-
-    def destroy(self, request, *args, **kwargs):
-        """Удаляет объект и проверяет доступ автора."""
-        instance = self.get_object()
-        response = self.handle_author_permission(instance, request)
-        if not response:
-            instance.delete()
-        return (
-            response or Response(status=status.HTTP_204_NO_CONTENT)
-        )
-
-
-class PostViewSet(AuthorPermissionMixin, viewsets.ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     """Управление объектами Post."""
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [
+        IsAuthorOrReadOnly]
     pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
@@ -56,11 +22,11 @@ class PostViewSet(AuthorPermissionMixin, viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
 
-class CommentViewSet(AuthorPermissionMixin, viewsets.ModelViewSet):
+class CommentViewSet(viewsets.ModelViewSet):
     """Управление объектами Comment."""
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [
+        IsAuthorOrReadOnly]
 
     def get_post_object_or_404(self):
         """Получает объект Post или возвращает ошибку 404."""
@@ -85,7 +51,6 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class FollowViewSet(mixins.CreateModelMixin,
@@ -93,7 +58,6 @@ class FollowViewSet(mixins.CreateModelMixin,
                     viewsets.GenericViewSet):
     """
     Представление для работы с подписками пользователей.
-
     Поддерживает операции создания и получения списка подписчиков .
     """
 
