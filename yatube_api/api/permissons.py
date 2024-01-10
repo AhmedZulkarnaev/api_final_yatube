cdfa01a15dc21cac62b3addc773284c00dc02ba4
Ahmed <ahmedzulkarnaev9500@gmail.com>
2024-01-10 19:58:42 +0300

    pep8

--- yatube_api/api/permissons.py
+++ yatube_api/api/permissons.py
@@ -2,8 +2,7 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    @staticmethod
-    def has_permission(request, view):
+    def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
             or request.user
@@ -11,6 +10,5 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
